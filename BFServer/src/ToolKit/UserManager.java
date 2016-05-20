3ecf2e755ce1b18095ef818b997508dbe2f60884
Silver Narcissus <827011867@qq.com>
2016-05-20 23:35:43 +0800

    登录界面基本建成

---
+++ BFServer/src/ToolKit/UserManager.java
@@ -0,0 +1,24 @@
+package toolKit;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class UserManager implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private ArrayList<UserInformation> userInformations = new ArrayList<UserInformation>();
+
+	public void addUserInformation(UserInformation userInfomation) {
+		userInformations.add(userInfomation);
+	}
+	public UserInformation getUserInfomation(String userName){
+		for(UserInformation userInformation:userInformations){
+			if(userInformation.getUserName().equals(userName)){
+				return userInformation;
+			}
+		}
+		return null;
+	}
+}
--- BFServer/src/serviceImpl/UserServiceImpl.java
+++ BFServer/src/serviceImpl/UserServiceImpl.java
@@ -1,18 +1,43 @@
 package serviceImpl;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.rmi.RemoteException;
 
 import service.UserService;
+import toolKit.UserInformation;
+import toolKit.UserManager;
 
 public class UserServiceImpl implements UserService {
+	UserManager userManager;
+
+	public UserServiceImpl() {
+		userManager = loadUserInformation();
+	}
+
 	@Override
-	public boolean creatNewUser(String username, String password) throws RemoteException {
-		return true;
+	public boolean creatNewUser(String userName, String password) throws RemoteException {
+		if (userManager.getUserInfomation(userName) == null) {
+			userManager.addUserInformation(new UserInformation(userName, password));
+			saveUserInformation();
+			return true;
+		}
+		return false;
 	};
 
 	@Override
-	public boolean login(String username, String password) throws RemoteException {
-		return true;
+	public boolean login(String userName, String password) throws RemoteException {
+		if(userManager.getUserInfomation(userName)!=null){
+			if(userManager.getUserInfomation(userName).getPassword().equals(password)){
+				return true;
+			}
+			else {
+				return false;
+			}
+		}
+		return false;
 	}
 
 	@Override
@@ -20,4 +45,26 @@ public class UserServiceImpl implements UserService {
 		return true;
 	}
 
+	private void saveUserInformation() {
+		try {
+			FileOutputStream fileOutputStream = new FileOutputStream("UserInformation.ser");
+			ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
+			objectOutputStream.writeObject(objectOutputStream);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+
+	private UserManager loadUserInformation() {
+		UserManager userManager = new UserManager();
+		try {
+			FileInputStream fileInputStream = new FileInputStream("UserInformation.ser");
+			ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
+			userManager = (UserManager) objectInputStream.readObject();
+			objectInputStream.close();
+		} catch (Exception ex) {
+			return userManager;
+		}
+		return userManager;
+	}
 }
